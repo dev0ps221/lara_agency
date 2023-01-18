bc8276760d277f23e201644a4491034efbdebd1b
dev0ps221 <dev0ps221@localhost.localdomain>
2023-01-18 22:10:36 +0000

    simple agency laravel website

---
+++ resources/views/notfound.php
@@ -0,0 +1,11 @@
+<section id='notfound'>
+    <h2>
+        Cette page n'existe pas.
+    </h2>
+    <p>
+        Soyez prudent, ne vous perdez pas en route !
+    </p>
+    <a href="/" style="color:white">
+        Cliquez ici pour aller vers l'accueil
+    </a>
+</section>
